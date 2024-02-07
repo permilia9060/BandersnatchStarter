8fdc7f7f574b203a9b8d8aed45be6829cddd5d27
Tom Tarpey <tom.tarpey@lambdaschool.com>
2024-02-07 14:40:57 -0500

    Update README.md Commiting the code for project BandersnatchStarter

--- app/data.py
+++ app/data.py
@@ -8,18 +8,36 @@ from pymongo import MongoClient
 
 
 class Database:
+    def __init__(self, db_name, collection_name):
+        self.client = MongoClient("mongodb+srv://permilia9060:Dillon9060!@cluster0.v88hr8e.mongodb.net/?retryWrites="
+                                  "true&w=majority")
+        self.db = self.client["BandersnatchStarter"]
+        self.collection = self.db["Collection"]
 
-    def seed(self, amount):
-        pass
+    def seed(self):
+        sample_data = [{"name": f"Monster {i}", "type": "Unknown"} for i in range(1, 1000 + 1)]
+        result = self.collection.insert_many(sample_data)
+        return len(result.inserted_ids)
 
     def reset(self):
-        pass
+        result = self.collection.delete_many({})
+        return result.deleted_count
 
     def count(self) -> int:
-        pass
+        return self.collection.count_documents({})
 
     def dataframe(self) -> DataFrame:
-        pass
+        cursor = self.collection.find()
+        data = list(cursor)
+        df = DataFrame(data)
+        return df
 
     def html_table(self) -> str:
-        pass
+        cursor = self.collection.find()
+        df = DataFrame(list(cursor))
+        html_table = df.to_html(index=False)
+        return html_table
+
+
+
+
